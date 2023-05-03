72eea1d97c5aaebd17f28c48fc53c8edf8c04a1b
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-03 15:40:10 +0200

    add typeseeder to database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
     {   
         $this->call([
             ProjectSeeder::class,
-            UserSeeder::class,
+            TypeSeeder::class,
+            UserSeeder::class
+            
         ]);
 
 

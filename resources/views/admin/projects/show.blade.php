da54407741c588402423f9db67ba9fed8ade0856
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-03 15:38:03 +0200

    add to show select type

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -13,15 +13,29 @@
   <section class="card clearfix mx-3">
     {{-- @dump($project) --}}
     <div class="card-body">
+      {{-- @dump($project->type?->label) --}}
       <figure class="float-end ms-5 mb-3">
         <img src="{{ $project->getImageUri() }}" alt="{{ $project->slug}}" width="200">
-      
-        <figcaption>
-          <p class="text-muted text-secondary m-0">{{ $project->slug}}</p>
-        </figcaption>
+          <figcaption>
+            <p class="text-muted text-secondary m-0">{{ $project->slug}}</p>
+          </figcaption>
       </figure>
-          <p>{{ $project->link}}</p>
-          <p>{{ $project->text}}</p>
+      <p>
+        <strong>Tipo:</strong>
+        <span class="badge rounded-pill" style="background-color: {{ $project->type?->color}} ">{{ $project->type?->label }}</span>
+      </p>
+
+      <p>
+        <strong>Contenuto:</strong>
+
+        <p>{{ $project->link}}</p>
+        <p>{{ $project->text}}</p>
+      </p>
+
+
+
+       
+          
     </div>
   </section>  
 @endsection
